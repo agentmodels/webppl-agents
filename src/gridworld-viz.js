5729e79955098f77561f63b09bc567b2213b9f3f
John Salvatier <jsalvatier@gmail.com>
2016-02-03 22:37:01 -0800

    add gridlines and refactor

--- src/gridworld-viz.js
+++ src/gridworld-viz.js
@@ -3,7 +3,7 @@ function createCanvas(element, options) {
   var canvas = document.createElement('canvas');
   canvas.width = options.width || 100;
   canvas.height = options.height || 100;
-  canvas.style.border = "1px solid #0f0";
+  canvas.style.border = "1px solid black";
   element.appendChild(canvas);
   return canvas;
 }
@@ -14,41 +14,40 @@ function draw(element, world, additional) {
 
 	var color = { 
 		blocked : 'grey',
-		terminal: new paper.Color(1,0,.2, .5),
+		terminal: new paper.Color(1,0,.2, .5), //transparent magenta
 		agent : 'blue'
 	 };
 
-	var scale = 20;
-	var scaleCoords = function (coords) { 
-		return [coords[0] * scale, coords[1] * scale];
-	};
-
 	var position = function(coords) {
-		return [coords[0] * scale, (world.yLim - coords[1]) * scale];
+		return [coords[0], world.yLim - coords[1]];
 	};
 
 	var makeSquare = function(c) {
-		var rect = new paper.Rectangle(scaleCoords([0, 0]), scaleCoords([1, 1]));
+		var rect = new paper.Rectangle(([0, 0]), ([1, 1]));
 		var path = new paper.Path.Rectangle(rect, .00);
 		path.fillColor = c;
 		path.remove();
 		return path;
 	};
 
-	var makeBackground = function(world) {
-		var rect = new paper.Rectangle(scaleCoords([-.5, .5]), scaleCoords([world.xLim, world.yLim]));
-		var path = new paper.Path.Rectangle(rect, .00);
-		path.fillColor = new paper.Color(1,1,1,0);
-		path.strokeColor = 'black';
-		path.remove();
-		return path;
+	var makeGrid = function(world) {
+		var group = new paper.Group();
+
+		for (var i = 0; i < world.xLim; i++) {
+			for (var j = 0; j < world.yLim; j++) {
+				var grid = new paper.Path.Rectangle(([i-.5, j+.5]), ([1, 1]));
+				grid.strokeColor = new paper.Color(.5, .2);
+				grid.fillColor = new paper.Color(1, 0);
+				group.addChild(grid);
+			}
+    }
+		return group;
 	};
 
 	var addShapes = function(coords, shape, group) {
 		for (var i = 0; i < coords.length; i++) {
 			var copy = shape.clone();
 			copy.position = position(coords[i]);
-			console.log(JSON.stringify(coords[i]) + JSON.stringify(copy.position));
 			group.addChild(copy);
 		}
 	};      
@@ -57,34 +56,30 @@ function draw(element, world, additional) {
 		var path = new paper.Path();
 
 		var coords = _.pluck(trajectory, 0);
-		var xy = _.map(coords, position);
-		path.addSegments(xy);
-		console.log(JSON.stringify(xy));
+		path.addSegments(_.map(coords, position));
 		path.strokeColor = 'black';
 		path.dashArray = [1,4];
 		group.addChild(path);
 
-		var agent = new paper.Path.Circle(position(coords[coords.length-1]), 4);
+		var agent = new paper.Path.Circle(position(coords[coords.length-1]), .3);
 		agent.fillColor = color.agent;
 		group.addChild(agent);
 	};
 
-	
-	var terminalSquare = makeSquare(color.terminal);
-	var square = makeSquare(color.blocked);
-
 	var group = new paper.Group();
-	group.addChild(makeBackground(world));
-	addShapes(world.blockedStates, square, group);
-	addShapes(world.terminals, terminalSquare, group);
+	//draw the world 
+	addShapes(world.blockedStates, makeSquare(color.blocked), group);
+	addShapes(world.terminals, makeSquare(color.terminal), group);
 
-	if (additional) {
+	group.addChild(makeGrid(world));
+
+	if (additional) { //additional items to be drawn
 		if (additional.trajectory) { 
 			addAgentPath(additional.trajectory, group);
 		}
 	}
-	group.fitBounds(paper.view.bounds);
 
+	group.fitBounds(paper.view.bounds);
   paper.view.draw();  
 }
 
