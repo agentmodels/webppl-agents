7c7c8f6bba5eae49a886c34e6eff64ab4aa0aa4f
John Salvatier <jsalvatier@gmail.com>
2016-02-09 12:17:06 -0800

    add run-in-html functionality

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # webppl-gridworld
 Webppl library for generating Gridworld MDPs. JS library for displaying Gridworld. 
+
+#Running script in browser
+
+Running a webppl script in the browser allows use of `GridWorld.draw` for easier debugging of agents. To do so, do 
+
+```
+sh compile.sh myscript.webppl
+```
---
+++ compile.sh
@@ -0,0 +1,23 @@
+#! /bin/bash 
+
+SCRIPT=$1
+
+WEBPPL_PATH=node_modules/webppl
+CURRENT_PATH=$(pwd)
+
+npm install webppl@latest 
+cd $WEBPPL_PATH
+npm install
+
+grunt compile:$CURRENT_PATH 
+cp compiled/webppl.js $CURRENT_PATH/runHtml/assets/js/
+
+cd $CURRENT_PATH
+
+#make an html file with the code from your script included
+sed "/@codehere@/{
+      s/@codehere@//g
+      r $SCRIPT
+}" runHtml/run.html > runHtml/$SCRIPT.html
+
+python -mwebbrowser file://$CURRENT_PATH/runHtml/$SCRIPT.html
