01ebc472d8baa26aa54a5d27ccece8ecfb796c15
John Salvatier <jsalvatier@gmail.com>
2016-02-18 14:59:25 -0800

    make agent not require delay

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
 SCRIPT=$1
+SCRIPT_FILENAME=$(echo "$SCRIPT" | rev | cut -d"/" -f1 | rev)
 CURRENT_PATH=$(pwd)
+
 
 #make an html file with the code from your script included
 sed "/@codehere@/{
       s/@codehere@//g
       r $SCRIPT
-}" runHtml/run.html > runHtml/$SCRIPT.html
+}" runHtml/run.html > runHtml/$SCRIPT_FILENAME.html
 
-python -mwebbrowser file://$CURRENT_PATH/runHtml/$SCRIPT.html
+python -mwebbrowser file://$CURRENT_PATH/runHtml/$SCRIPT_FILENAME.html
