b50618b75b5c0fcf6e483597a416f8da00268229
John Salvatier <jsalvatier@gmail.com>
2016-03-02 16:22:03 -0800

    add test script

---
+++ test.sh
@@ -0,0 +1,13 @@
+#! /bin/bash 
+
+TEST_LOCATION=${1-tests}
+CURRENT_PATH=$(pwd)
+REQUIRES="--require webppl-timeit --require webppl-dp --require $CURRENT_PATH"
+
+TEST_FILES=$(find $TEST_LOCATION -type f )
+
+for testfile in $TEST_FILES 
+do
+  webppl $testfile $REQUIRES || { echo "$testfile failed" ; exit 1; }
+done
+
