9049e1e61100fabeecd89f98a30f4ebc1ae14c0f
Owain Evans <owain@mit.edu>
2016-05-10 22:54:31 +0100

    Delete testGrid.sh

--- tests/testGrid.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash 
-
-
-webppl tests/makeGridworld.wppl --require webppl-dp --require webppl-timeit --require .
-webppl tests/beliefGridworld.wppl --require webppl-dp --require webppl-timeit --require .
-webppl tests/restaurantChoiceHyperbolic.wppl --require webppl-dp --require webppl-timeit --require .
-
