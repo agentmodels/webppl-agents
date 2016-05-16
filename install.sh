ff6c7b34ce3bd5c1b7f24f775230aa3969771335
John Salvatier <jsalvatier@gmail.com>
2016-05-16 03:56:06 -0700

    move dependencies inside the repo

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
-git clone https://github.com/probmods/webppl.git ../webppl
-git clone https://github.com/stuhlmueller/webppl-timeit.git ../webppl-timeit
-git clone https://github.com/stuhlmueller/webppl-dp.git ../webppl-dp
-git clone https://github.com/probmods/webppl-viz ../webppl-viz
-npm link ../webppl-timeit 
-npm link ../webppl-dp 
-npm link ../webppl-viz
-npm link ../webppl 
+DEPS=webppl-dependencies
+mkdir $DEPS
+git clone https://github.com/probmods/webppl.git            $DEPS/webppl
+git clone https://github.com/stuhlmueller/webppl-timeit.git $DEPS/webppl-timeit
+git clone https://github.com/stuhlmueller/webppl-dp.git     $DEPS/webppl-dp
+git clone https://github.com/probmods/webppl-viz            $DEPS/webppl-viz
+npm link $DEPS/webppl-timeit 
+npm link $DEPS/webppl-dp 
+npm link $DEPS/webppl-viz
+npm link $DEPS/webppl 
 npm install grunt uglify browserify
 npm install -g grunt-cli 
 npm install
