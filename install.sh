356d2a89a0b23927827f1456c46bbbfd61e87dcb
John Salvatier <jsalvatier@gmail.com>
2016-05-16 04:01:44 -0700

    install cmd line installation

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo "installing dependencies for running in the browser"
 DEPS=webppl-dependencies
 mkdir $DEPS
 git clone https://github.com/probmods/webppl.git            $DEPS/webppl
@@ -11,3 +12,9 @@ npm link $DEPS/webppl
 npm install grunt uglify browserify
 npm install -g grunt-cli 
 npm install
+
+echo "Installing dependencies for running from the command line"
+mkdir ~/.webppl
+npm install --prefix ~/.webppl https://github.com/stuhlmueller/webppl-dp
+npm install --prefix ~/.webppl https://github.com/stuhlmueller/webppl-timeit
+npm install --prefix ~/.webppl https://github.com/probmods/webppl-viz
